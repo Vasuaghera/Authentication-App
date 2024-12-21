8cfba21111278d4e919e36fd2476d43b33015a6e
Vasu Aghera <120328181+Vasuaghera@users.noreply.github.com>
2024-12-21 14:49:40 +0530

    Add files via upload

---
+++ Frontend/src/context/MainContext.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export const MainContext = createContext() ;
+
+export const MainContextProvider = (props) => {
+    
+    axios.defaults.withCredentials = true ;
+    const BackendUrl = import.meta.env.VITE_BACKEND_URL ;
+    const [isLoggedIn , setIsLoggedIn] = useState(false) ;
+    const [userData , setUserData] = useState(false) ;
+
+    const getUserData = async() => {
+        try {
+            const {data} = await axios.get(BackendUrl+'/api/user/data') ;
+            data.success ? setUserData(data.userData) : toast.error(data.message) ;
+        }
+        catch(error){
+            toast.error(error.message) ;
+        }
+    }
+
+    const getAuthState = async() => {
+        try{
+            const {data} = await axios.get(BackendUrl + '/api/auth/is-auth') ;
+            if(data.success) {
+                setIsLoggedIn(true) ;
+                getUserData() ;
+            }
+
+        }
+        catch(error) {
+            toast.error(error.message) ;
+        }
+    }
+
+    useEffect(() => {
+        getAuthState() ;
+    },[])
+
+    const value = {
+        BackendUrl ,
+        isLoggedIn , setIsLoggedIn ,
+        userData , setUserData ,
+        getUserData
+    }
+    
+    return (
+        <MainContext.Provider value={value}> 
+            {props.children}
+        </MainContext.Provider> 
+    )
+}
