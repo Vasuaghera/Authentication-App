bc9b2983047c9d0c6360d3c1a975b6849f97c21b
Vasu Aghera <120328181+Vasuaghera@users.noreply.github.com>
2025-06-02 16:39:23 +0530

    Delete Backend directory

--- Backend/routes/userDetailRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userAuth from "../middleware/userAuth.js";
-import { getUserData } from "../controllers/userDetail.js";
-
-const userDetailRoute = express.Router() ;
-
-userDetailRoute.get('/data',userAuth , getUserData);
-
-export default userDetailRoute ;
